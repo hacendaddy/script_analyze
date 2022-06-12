f19939e4c30ea0c1ee9143ca0ea930a944a1cabb
Pau Camós <paucapaofficial@gmail.com>
2022-06-12 18:09:38 +0200

    Optimized utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -39,9 +39,8 @@ def join_male_female(path: str, year: int) -> pd.DataFrame:
     all_filenames = [i for i in glob.glob(new_path)]
     csvs = []
     for i in all_filenames:
-        temp = pd.read_csv(i, low_memory=False)
-        temp['gender'] = 'M' if 'female' not in i else 'F'
-        temp['year'] = int(f'20{year}')
+        gender = 'M' if 'female' not in i else 'F'
+        temp = read_add_year_gender(i, gender, year)
         csvs.append(temp)
 
     return pd.concat(csvs)
@@ -52,25 +51,14 @@ def join_datasets_year(path: str, years: list) -> pd.DataFrame:
     - path: ruta a la carpeta que conté les dades
     - years: llista d’anys que es volen incloure en el dataframe, en format [XXXX,...]
     """
-    all_filenames = []
-    for year in years:
-        year = get_small_year(year)
-        new_path = os.path.join(f"{path}", f'*{year}.csv')
-        all_filenames.append(list(glob.glob(new_path)))
-
     csvs = []
-    for fi_name in all_filenames:
-        for i in fi_name:
-            temp = pd.read_csv(i, low_memory=False)
-            temp['gender'] = 'M' if 'female' not in i else 'F'
-            year = ''.join(filter(str.isdigit, i))
-            temp['year'] = int(f'20{year}')
-            csvs.append(temp)
+
+    for year in years:
+        gender = join_male_female(path, year)
+        csvs.append(gender)
 
     return pd.concat(csvs)
 
 
 if __name__ == "__main__":
     print("This is utils module")
-    # print(join_male_female('data', 2020).shape)
-    # join_datasets_year('data/', [2020, 2021])
